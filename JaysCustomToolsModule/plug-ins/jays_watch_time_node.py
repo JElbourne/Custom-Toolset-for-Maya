9235a0cebe8d2b747334f6437ef5efffb768cf3f
Jay Elbourne <jayelbourne@gmail.com>
2020-03-20 01:44:17 -0400

    Updated the Watch Node plugin

--- JaysCustomToolsModule/plug-ins/jays_watch_time_node.py
+++ JaysCustomToolsModule/plug-ins/jays_watch_time_node.py
@@ -25,6 +25,9 @@ class JaysWatchTimeNode(om.MPxNode):
     new_sec_hand = None
     new_min_hand = None
     new_hour_hand = None
+    
+    man_min_hand = None
+    man_hour_hand = None
 
     def __init__(self):
         super(JaysWatchTimeNode, self).__init__()
@@ -34,47 +37,59 @@ class JaysWatchTimeNode(om.MPxNode):
             frame = data.inputValue(JaysWatchTimeNode.frame_time).asDouble()
             curr_sec_rot = data.inputValue(JaysWatchTimeNode.curr_sec_hand).asDouble()
             
-            seconds = frame / 24.0
+            sec_rotation = 0.0
             
-            if (seconds % 1) == 0:
-                rotation_in_rads = curr_sec_rot - (6.0 * 0.01745)
-            else:
-                rotation_in_rads = curr_sec_rot
+            if not frame <= 1:
+                seconds = frame / 24.0
+                if (seconds % 1) == 0:
+                    sec_rotation = (-6 * seconds) * 0.01745
+                else:
+                    sec_rotation = curr_sec_rot
             
             new_sec_rot = data.outputValue(JaysWatchTimeNode.new_sec_hand)
-            new_sec_rot.setDouble(rotation_in_rads)
+            new_sec_rot.setDouble(sec_rotation)
             
             data.setClean(plug)
             
         elif plug == JaysWatchTimeNode.new_min_hand:
             frame = data.inputValue(JaysWatchTimeNode.frame_time).asDouble()
             curr_min_rot = data.inputValue(JaysWatchTimeNode.curr_min_hand).asDouble()
+            man_min_rot = data.inputValue(JaysWatchTimeNode.man_min_hand).asDouble()
+            manual_min_offset = man_min_rot * 0.01745
             
-            seconds = frame / 24.0
-            
-            if (seconds % 60) == 0:
-                rotation = curr_min_rot - (6.0 * 0.01745)
+            if frame <= 1:
+                min_rotation = 0.0 - manual_min_offset
             else:
-                rotation = curr_min_rot
+                seconds = frame / 24.0
+                
+                if (seconds % 60) == 0:
+                    min_rotation = ((-6.0 * (seconds/60)) * 0.01745) - manual_min_offset
+                else:
+                    min_rotation = curr_min_rot
             
             new_min_rot = data.outputValue(JaysWatchTimeNode.new_min_hand)
-            new_min_rot.setDouble(rotation)
+            new_min_rot.setDouble(min_rotation)
             
             data.setClean(plug)
             
         elif plug == JaysWatchTimeNode.new_hour_hand:
             frame = data.inputValue(JaysWatchTimeNode.frame_time).asDouble()
             curr_hour_rot = data.inputValue(JaysWatchTimeNode.curr_hour_hand).asDouble()
+            man_hour_rot = data.inputValue(JaysWatchTimeNode.man_hour_hand).asDouble()
+            manual_offset = man_hour_rot * 0.01745
             
-            seconds = frame / 24.0
-            
-            if (seconds % 3600) == 0:
-                rotation = curr_hour_rot - (30.0 * 0.01745)
+            if frame <= 1:
+                hour_rotation = 0.0 - manual_offset
             else:
-                rotation = curr_hour_rot
+                seconds = frame / 24.0
+                
+                if (seconds % 3600) == 0:
+                    hour_rotation = ((-30.0 * (seconds/3600)) * 0.01745) - manual_offset
+                else:
+                    hour_rotation = curr_hour_rot
                         
             new_hour_rot = data.outputValue(JaysWatchTimeNode.new_hour_hand)
-            new_hour_rot.setDouble(rotation)
+            new_hour_rot.setDouble(hour_rotation)
             
             data.setClean(plug)
 
@@ -104,6 +119,14 @@ class JaysWatchTimeNode(om.MPxNode):
         unit_attr.keyable= True
         unit_attr.readable = False
         
+        cls.man_min_hand = numeric_attr.create("man_min_rotation", "mmr", om.MFnNumericData.kDouble, 0.0)
+        numeric_attr.keyable= True
+        numeric_attr.readable = False
+        
+        cls.man_hour_hand = numeric_attr.create("man_hour_rotation", "mhr", om.MFnNumericData.kDouble, 0.0)
+        numeric_attr.keyable= True
+        numeric_attr.readable = False
+        
         cls.new_sec_hand = unit_attr.create("new_sec_rot", "nsr", om.MFnUnitAttribute.kAngle, 0.0)
         cls.new_min_hand = unit_attr.create("new_min_rot", "nmr", om.MFnUnitAttribute.kAngle, 0.0)
         cls.new_hour_hand = unit_attr.create("new_hour_rot", "nhr", om.MFnUnitAttribute.kAngle, 0.0)
@@ -115,6 +138,8 @@ class JaysWatchTimeNode(om.MPxNode):
         cls.addAttribute(cls.new_sec_hand)
         cls.addAttribute(cls.new_min_hand)
         cls.addAttribute(cls.new_hour_hand)
+        cls.addAttribute(cls.man_min_hand)
+        cls.addAttribute(cls.man_hour_hand)
         
         cls.attributeAffects(cls.frame_time, cls.new_sec_hand)
         cls.attributeAffects(cls.frame_time, cls.new_min_hand)
@@ -123,6 +148,9 @@ class JaysWatchTimeNode(om.MPxNode):
         cls.attributeAffects(cls.curr_sec_hand, cls.new_sec_hand)
         cls.attributeAffects(cls.curr_min_hand, cls.new_min_hand)
         cls.attributeAffects(cls.curr_hour_hand, cls.new_hour_hand)
+        
+        cls.attributeAffects(cls.man_min_hand, cls.new_min_hand)
+        cls.attributeAffects(cls.man_hour_hand, cls.new_hour_hand)
 
 
 def initializePlugin(plugin):
